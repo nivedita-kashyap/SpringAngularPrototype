78ce115d95fedd138c9724dc3b4d3574d3ed339c
shaila.p <shaila.p@desktop-050.mportalindia.com>
2015-04-08 12:01:50 +0530

    1.included logo 2.logout changes

--- SpringAngularPrototype/src/main/webapp/resources/js/controllers/LoginControler.js
+++ SpringAngularPrototype/src/main/webapp/resources/js/controllers/LoginControler.js
@@ -1,25 +1,25 @@
 'use strict';
 var LoginController = function($scope, $location, $window) {
 	// page.setPage("Login","login-layout");
-	 $scope.user = {};
-	 $scope.isLogin = false;
-	 $scope.error = false
-	$scope.login=function()
-    {
+	$scope.user = {};
+	$scope.isLogin = false;
+	$scope.error = false
+	$scope.login = function() {
 		var username = $scope.username;
 		var password = $scope.password;
-        if(username=="admin" && password=="admin123")
-        {
-           /* page.setUser($scope.user);*/
-            $location.path( "/cars" );
-        	$scope.isLogin = true;
-        }
-        else
-        {
-        	$location.path( "/login" );
-            $scope.error=true;
-        	$scope.isLogin = false;
-        }
-    }
+		if (username == "admin" && password == "admin123") {
+			/* page.setUser($scope.user);*/
+			$location.path("/cars");
+			$scope.isLogin = true;
+		} else {
+			$location.path("/login");
+			$scope.error = true;
+			$scope.isLogin = false;
+		}
+	}
+	$scope.logout = function() {
+		$scope.isLogin = false;
+		$scope.username = "";
+		$scope.password = "";		
+	}
 };
-
